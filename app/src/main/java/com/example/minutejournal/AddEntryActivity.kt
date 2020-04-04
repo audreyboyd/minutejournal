449e258a9c8c3033043d192f88bbb2eb668e4caa
jacob <54485488+mastroja@users.noreply.github.com>
2020-04-04 14:25:02 -0400

    code review 2
    mostly ui changes

--- app/src/main/java/com/example/minutejournal/AddEntryActivity.kt
+++ app/src/main/java/com/example/minutejournal/AddEntryActivity.kt
@@ -4,19 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-
+class AddEntryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        setContentView(R.layout.activity_add_entry)
 
-        val button = findViewById<ImageView>(R.id.numberOK)
+        val button = findViewById<TextView>(R.id.doneBtn)
         button.setOnClickListener {
             // Handler code here.
             val intent = Intent(this, EntriesActivity::class.java)
             startActivity(intent);
+
+        }
+
+        val button2 = findViewById<TextView>(R.id.cancelBtn)
+        button2.setOnClickListener {
+            // Handler code here.
+            val intent = Intent(this, EntriesActivity::class.java)
+            startActivity(intent);
+
         }
     }
 }
